a5d7dc86d10c7bbf9a1fb34549575c3ca529555c
Sam Josh <sam.samudio@grangedevelopment.com>
2025-01-21 08:16:38 +0800

    edit
    edit

--- resources/qgis2web.js
+++ resources/qgis2web.js
@@ -219,73 +219,58 @@ function onPointerMove(evt) {
     
 	if (doHighlight) {
         if (currentFeature !== highlight) {
-            // Check if highlight is defined and exists in the source before removing
-            if (highlight && featureOverlay.getSource().getFeatures().includes(highlight)) {
+            if (highlight) {
                 featureOverlay.getSource().removeFeature(highlight);
             }
             if (currentFeature) {
-                var featureStyle;
+                var featureStyle
                 if (typeof clusteredFeatures == "undefined") {
-                    var style = currentLayer.getStyle();
-                    var styleFunction = typeof style === 'function' ? style : function() { return style; };
-                    featureStyle = styleFunction(currentFeature)[0];
-                } else {
-                    featureStyle = currentLayer.getStyle().toString();
-                }
-    
+					var style = currentLayer.getStyle();
+					var styleFunction = typeof style === 'function' ? style : function() { return style; };
+					featureStyle = styleFunction(currentFeature)[0];
+				} else {
+					featureStyle = currentLayer.getStyle().toString();
+				}
+
                 if (currentFeature.getGeometry().getType() == 'Point' || currentFeature.getGeometry().getType() == 'MultiPoint') {
-                    var radius = featureStyle.getImage().getRadius();
-                    
+                    var radius
+					if (typeof clusteredFeatures == "undefined") {
+						radius = featureStyle.getImage().getRadius();
+					} else {
+						radius = parseFloat(featureStyle.split('radius')[1].split(' ')[1]) + clusterLength;
+					}
+
                     highlightStyle = new ol.style.Style({
                         image: new ol.style.Circle({
                             fill: new ol.style.Fill({
-                                color: "#ffff00"  // Yellow fill for point highlight
+                                color: "#ffff00"
                             }),
                             radius: radius
                         })
-                    });
+                    })
                 } else if (currentFeature.getGeometry().getType() == 'LineString' || currentFeature.getGeometry().getType() == 'MultiLineString') {
-                    
+
                     var featureWidth = featureStyle.getStroke().getWidth();
-                    
+
                     highlightStyle = new ol.style.Style({
                         stroke: new ol.style.Stroke({
-                            color: '#ffff00',   // Yellow color for line highlight
+                            color: '#ffff00',
                             lineDash: null,
                             width: featureWidth
                         })
                     });
-                } else {  // Polygon or MultiPolygon
+
+                } else {
                     highlightStyle = new ol.style.Style({
-                        stroke: new ol.style.Stroke({
-                            color: '#ffff00',  // Outline color for polygon highlight
-                            width: 2           // Adjust width as needed
-                        }),
                         fill: new ol.style.Fill({
-                            color: 'rgba(255, 255, 255, 0)'  // Transparent fill for polygon
+                            color: '#ffff00'
                         })
-                    });
+                    })
                 }
-                
-                // Add the highlighted feature to the overlay and apply the style
                 featureOverlay.getSource().addFeature(currentFeature);
                 featureOverlay.setStyle(highlightStyle);
-                highlight = currentFeature;
             }
-        }
-    }
-    
-    
-    }
-
-    if (doHover) {
-        if (popupText) {
-            overlayPopup.setPosition(coord);
-            content.innerHTML = popupText;
-            container.style.display = 'block';        
-        } else {
-            container.style.display = 'none';
-            closer.blur();
+            highlight = currentFeature;
         }
     }
 
@@ -297,8 +282,9 @@ function onPointerMove(evt) {
         } else {
             container.style.display = 'none';
             closer.blur();
-        }
-    };
+        }
+    }
+};
 
 map.on('pointermove', onPointerMove);
 
